75ef161b94b714a24c1766f3a7db40ff64cd56e1
Mario <mnarcila@gmail.com>
2021-05-30 23:01:33 -0500

    adding 'hola' and 'init' command

---
+++ server.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = '-timer'
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+
+client.on('ready', () => {
+  console.log(`BOT LISTO`);
+});
+
+client.on('message', message => {
+  var args = message.content.substring(prefix.length).split(' ');
+  var command = args[1];
+  if (message.content.substring(0, prefix.length) == prefix) {
+    
+    if (command === 'hola') {
+      message.reply('Hola amiguitos!');
+    }
+    if (command === 'init') {
+      var intervalCount = 0, interval = 1;
+      message.reply('bot iniciado.');
+      setPart(interval);
+
+      function setPart(interval) {
+        intervalCount++;
+        interval = 1;
+        let timer = setTimeout(() => {
+          message.channel.send(`Vuelta ${intervalCount},
+            ${interval} minuto finalizado.`);
+          setPart();
+        }, interval * minute);
+        if (intervalCount >= 10) {
+            message.reply('Final ciclos');
+            clearTimeout(timer);
+            return;
+        }
+      }
+    }
+  }
+});
+
+client.login('ODQ4NzQxNzEyODI5ODc0MjE3.YLRCUQ.T8TOS9oDh2pO3CwGNw3SYuPMQlc');
